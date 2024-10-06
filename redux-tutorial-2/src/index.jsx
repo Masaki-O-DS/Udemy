0da55cecde0fe23f6f78961e1520251aeba3d4bc
Masaki Ogawa <pataporasu@gmail.com>
2024-10-06 16:43:40 +0900

    done

--- redux-tutorial-2/src/index.jsx
+++ redux-tutorial-2/src/index.jsx
@@ -3,44 +3,22 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { createStore } from "redux";
+import counterReducer from "./reducers/counter.js";
+import isLoginReducer from "./reducers/isLogin.js";
+import allReducers from "./reducers/index.js";
+import { Provider } from "react-redux";
 
-//reducer
-const counterReducer = (state = 0, action) => {
-  switch (action.type) {
-    case "INCREMENT":
-      return state + 1;
-    case "DECREMENT":
-      return -1;
-    default:
-      return state;
-  }
-};
-
-//store
-let store = createStore(counterReducer);
-
-//actions    increment decrement
-const increment = () => {
-  return {
-    type: "INCREMENT",
-  };
-};
-const decrement = () => {
-  return {
-    type: "DECREMENT",
-  };
-};
-
-//コンソールに新しい状態を出力
-store.subscribe(() => console.log(store.getState()));
-
-//dispatch
-store.dispatch(increment());
-store.dispatch(increment());
+const store = createStore(
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+); //この第２引数のおかげで開発者ツールでstateを見ることができる
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* / このproviderで全てのコンポーネントにアクセスできるようになる */}
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
